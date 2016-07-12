f76efcc9f0267ffd034a9ca2258b3fd09ee0f5cc
wusy1220 <957562677@qq.com>
2016-07-12 11:47:56 +0800

    初始化工程

---
+++ testlib/src/main/java/cn/wsy/testlib/Tools.java
@@ -0,0 +1,14 @@
+package cn.wsy.testlib;
+
+import android.util.Log;
+
+/**
+ * Created by wsy on 2016/7/12.
+ */
+public class Tools {
+
+    public static void out(){
+        Log.i("wusy","that is test lib");
+    }
+
+}
